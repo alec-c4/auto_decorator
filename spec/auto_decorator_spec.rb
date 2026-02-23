df48adadb751135803d3b1ea7a2f1c58e1c64f2f
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2026-02-23 20:58:14 +0300

    chore: remove scaffold placeholder tests

--- spec/auto_decorator_spec.rb
+++ spec/auto_decorator_spec.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 RSpec.describe AutoDecorator do
-  it "has a version number" do
-    expect(AutoDecorator::VERSION).not_to be_nil
-  end
-
-  it "is loadable" do
-    expect(defined?(described_class)).to be_truthy
-  end
 end
