260c6b7ff5b4e1d366fd53a0df571616bfa70842
David Rybolovlev <i@golifox.ru>
2026-03-16 17:22:20 +0300

    refactor: simplify generator command to `rails g decorator`
    Remove AutoDecorator namespace wrapper from DecoratorGenerator to enable
    shorter command invocation. Changes `rails g auto_decorator:decorator Model`
    to `rails g decorator Model`.

---
+++ lib/generators/decorator/decorator_generator.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails/generators"
+
+class DecoratorGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("templates", __dir__)
+
+  def create_decorator_file
+    template "decorator.rb.tt",
+      File.join("app/decorators", class_path.join("/"), "#{file_name}_decorator.rb")
+  end
+
+  private
+
+  def decorator_name
+    "#{class_name.demodulize}Decorator"
+  end
+
+  def namespaces
+    class_name.deconstantize.split("::").reject(&:empty?)
+  end
+end
